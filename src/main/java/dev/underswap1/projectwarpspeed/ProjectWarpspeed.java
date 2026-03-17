c1f09695a9f405682c9e641bc1bc57ba2a90fc05
Underswap1 <levivanhorck@gmail.com>
2026-03-18 00:25:01 +0100

    regertering AlloyGenerator.java

--- src/main/java/dev/underswap1/projectwarpspeed/ProjectWarpspeed.java
+++ src/main/java/dev/underswap1/projectwarpspeed/ProjectWarpspeed.java
@@ -1,5 +1,6 @@
 package dev.underswap1.projectwarpspeed;
 
+import dev.underswap1.projectwarpspeed.materials.AlloyGenerator;
 import dev.underswap1.projectwarpspeed.materials.MaterialCommands;
 import dev.underswap1.projectwarpspeed.registry.MaterialRegistry;
 import dev.underswap1.projectwarpspeed.registry.ProjectWarpspeedBiomes;
@@ -11,6 +12,7 @@ import org.slf4j.LoggerFactory;
 public class ProjectWarpspeed implements ModInitializer {
     public static final String MOD_ID = "project_warpspeed";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static AlloyGenerator ALLOY_GENERATOR;
 
     @Override
     public void onInitialize() {
@@ -19,6 +21,7 @@ public class ProjectWarpspeed implements ModInitializer {
         ProjectWarpspeedBiomes.registerAll();
         ProjectWarpspeedBiomes.createBiomes();
         MaterialRegistry.loadMaterials();
+        ALLOY_GENERATOR = new AlloyGenerator();
         MaterialCommands.registerCommands();
     }
 }
