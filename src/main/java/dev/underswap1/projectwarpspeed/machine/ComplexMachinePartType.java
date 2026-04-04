aff25de4e06e3f1a9eb078e9e9ca7fe738c25e0d
Underswap1 <levivanhorck@gmail.com>
2026-04-04 15:07:40 +0200

    list of part that will be implemented later

---
+++ src/main/java/dev/underswap1/projectwarpspeed/machine/ComplexMachinePartType.java
@@ -0,0 +1,34 @@
+package dev.underswap1.projectwarpspeed.machine;
+
+@SuppressWarnings("unused")
+public enum ComplexMachinePartType {
+    GEARBOX,
+    MOTOR,
+    GENERATOR,
+    TURBINE,
+    PUMP,
+    COMPRESSOR,
+    FILTER,
+    FAN,
+    REACTOR,
+    CHAMBER,
+    CONTROL_UNIT,
+    DRIVE_ASSEMBLY,
+    POWER_COUPLER,
+    PRESSURE_VESSEL,
+    VACUUM_CHAMBER,
+    COMBUSTION_CHAMBER,
+    CONDENSER,
+    SEPARATOR,
+    SCRUBBER,
+    DISTILLER,
+    ABSORBER,
+    PRECIPITATOR,
+    CONVEYOR,
+    FEEDER,
+    HOPPER,
+    ACTUATOR,
+    SERVO,
+    SENSOR,
+    CONTROLLER
+}
