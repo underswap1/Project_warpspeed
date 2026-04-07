d81bf2a323152810b8fde3f13a9d56e17ff4e513
Underswap1 <levivanhorck@gmail.com>
2026-04-07 17:30:28 +0200

    screen handeler

--- src/main/java/dev/underswap1/projectwarpspeed/client/ProjectWarpspeedClient.java
+++ src/main/java/dev/underswap1/projectwarpspeed/client/ProjectWarpspeedClient.java
@@ -1,12 +1,21 @@
 package dev.underswap1.projectwarpspeed.client;
 
 import dev.underswap1.projectwarpspeed.ProjectWarpspeed;
+import dev.underswap1.projectwarpspeed.registry.ProjectWarpspeedScreenHandlers;
+import dev.underswap1.projectwarpspeed.screen.MachineScreen;
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.gui.screen.ingame.HandledScreens;
 
 public class ProjectWarpspeedClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ProjectWarpspeed.LOGGER.info("Project Warpspeed client initialized!");
-        // Register sky renderer or client-only features here later
+        // Register sky renderer or client-only features here
+        HandledScreens.register(
+                ProjectWarpspeedScreenHandlers.MACHINE,
+                MachineScreen::new
+        );
     }
+
+
 }
